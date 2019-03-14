07f34de3252a06e8de9a41cc006112afd7439ac8
Victor Ajani <portneufcoder@gmail.com>
2019-03-13 21:38:50 -0400

    initial project files

---
+++ CityCM/CityCM.BL/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityCM.BL
+{
+    public class Customer
+    {
+        public int CustomerId { get; private set; }
+        public string EmailAddress { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string FullName
+        {
+            get { return LastName + "," + FirstName; }
+        }
+        
+        private string _lastName;
+
+        public string LastName
+        {
+            get { return _lastName; }
+
+            set { _lastName = value; }
+        }
+        
+    }
+}
